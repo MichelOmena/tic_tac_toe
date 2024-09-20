19fd544daa65e40f57018d9da5bf32b70b55091d
Michel Omena <micheltenoriodeomena@hotmail.com>
2024-09-20 22:14:34 +0100

    update link cdn to local

--- index.php
+++ index.php
@@ -5,3 +5,29 @@ session_start();
 
 //define control variable 
 define('CONTROL', true);
+
+//define routes
+$routes = $_GET['route'] ?? 'start';
+
+$script = null;
+
+switch ($route) {
+    case 'start':
+        $script = 'start.php';
+        break;
+    case 'game':
+        $script = 'game.php';
+    case 'end':
+        $script = 'end.php';
+    case 'game':
+        $script = 'game.php';
+        break;
+    default:
+    $script = '404.php';
+    break;
+}
+
+//view
+require "in/header.php";
+require "inc/$script";
+require "inc/footer.php";
